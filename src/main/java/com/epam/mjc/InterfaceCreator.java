f1d235744862129587efd12914c7db29d5b8886a
Denis Stepshin <Evil9493@ya.ru>
2023-08-07 21:35:13 +0600

    Complete

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,16 @@
 package com.epam.mjc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> list.stream().map(i -> i/3).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        var interfaceCreator = new InterfaceCreator();
+        System.out.println(interfaceCreator.divideBy(3).apply(List.of(30, 60, 90, 100)));
     }
 }
